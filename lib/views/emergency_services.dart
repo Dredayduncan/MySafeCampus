bd982cf63a9d9fda15e5394ac1d0a8bfcb5e5468
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2022-05-24 21:40:55 +0000

    new files added

---
+++ lib/views/emergency_services.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmergencyServices extends StatefulWidget {
+  const EmergencyServices({Key? key}) : super(key: key);
+
+  @override
+  State<EmergencyServices> createState() => _EmergencyServicesState();
+}
+
+class _EmergencyServicesState extends State<EmergencyServices> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData icon;
+  final Function(String) onChanged;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.onChanged,
+    required this.validator,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      onChanged: onChanged,
+      validator: validator,
+      style: const TextStyle(color: kDarkTextColor),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.w600,
+        ),
+        prefixIcon: Icon(
+          icon,
+          color: Colors.grey,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey.withOpacity(0.3)),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey.withOpacity(0.3)),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.withOpacity(0.3),
+          ),
+        ),
+      ),
+    );
+  }
+}
