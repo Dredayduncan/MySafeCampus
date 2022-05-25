c4fa8ee554dac2c7713d9bf3f889c2dd0ba1f4d6
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2022-05-25 10:10:01 +0000

    blogs and articles with some changes, completed

---
+++ lib/model/blog_model.dart
@@ -0,0 +1,68 @@
+class BlogModel {
+  final String? image;
+  final String? name;
+  final String? duration;
+  final String? title;
+  final String? content;
+
+  BlogModel(this.image, this.name, this.duration, this.title, this.content);
+}
+
+List<BlogModel> blogItems = [
+  BlogModel(
+    "image",
+    "Allison Rhiel Madsen",
+    "5 min read",
+    "The way I dress is not a yes",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Lincoln Rhiel Madsen",
+    "5 min read",
+    "Silence does not mean consent",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Allison Rhiel Madsen",
+    "5 min read",
+    "The way I dress is not a yes",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Lincoln Rhiel Madsen",
+    "5 min read",
+    "Silence does not mean consent",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Allison Rhiel Madsen",
+    "5 min read",
+    "The way I dress is not a yes",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Lincoln Rhiel Madsen",
+    "5 min read",
+    "Silence does not mean consent",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Allison Rhiel Madsen",
+    "5 min read",
+    "The way I dress is not a yes",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+  BlogModel(
+    "image",
+    "Lincoln Rhiel Madsen",
+    "5 min read",
+    "Silence does not mean consent",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet posuere feugiat lorem. Magna urna ut blandit adipiscing. Enim turpis vitae vel netus tincidunt massa pellentesque. Senectus sagittis pellentesque velit non egestas nulla eget consectetur. Quis amet est hendrerit ac commodo.",
+  ),
+];
